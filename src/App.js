82e19dca8d00e6989fa91c505debd2bc7ad2eb46
Kenton Lam <kenton_lam@outlook.com>
2019-11-28 20:25:22 +1000

    Working on first screen, using bulma.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="section">
+      <div className="columns is-centered is-mobile">
+        <div className="column main-col">
+          <div className="box">
+            <h1 className="is-size-3 has-text-weight-bold">Yeetbeat 2</h1>
+            A music guessing game!
+            <div className="columns is-multiline">
+              <div className="column"><div className="button is-fullwidth is-success">Join Game</div></div>
+              <div className="column"><div className="button is-fullwidth is-info">New Game</div></div>
+              <div className="column"><div className="button is-fullwidth is-info">Settings</div></div>
+            </div>
+          </div>
+        </div>
+      </div>      
     </div>
   );
 }
